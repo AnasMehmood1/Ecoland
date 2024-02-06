25eb573ab317f54937885ad4b7b4b2d917045ea0
AnasMehmood1 <anasmehmood167@gmail.com>
2024-02-06 17:39:20 +0500

    Working

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import './App.css';
 import Navbar from './Component/Navbar/Navbar';
 import Check from './Component/Checkin/Check';
+import AboutSec from './Component/AboutSec/AboutSec';
 
 function App() {
   return <>
      <Navbar/>
      <Check/>
+     <AboutSec/>
    
   </>
 }
---
+++ src/Component/AboutSec/AboutSec.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./AboutSec.css"
+const AboutSec = () => {
+  return (
+    <>
+      <div className="about-sec-container">
+        <div className="about-left"></div>
+        <div className="about-left-left">
+            <div className="detail">
+                
+            </div>
+        </div>
+        <div className="about-right"></div>
+      </div>
+    </>
+  )
+}
+
+export default AboutSec
