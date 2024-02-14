c05be6241b4338e616635e1a7bf818836fb30479
AnasMehmood1 <anasmehmood167@gmail.com>
2024-02-14 14:28:00 +0500

    working

--- src/Component/ContactSec/ContactSec.jsx
+++ src/Component/ContactSec/ContactSec.jsx
@@ -45,7 +45,7 @@ const ContactSec = () => {
             </div>
             
             <div className="contact-right">
-            <iframe title='map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14440.712447076416!2d55.2559224640452!3d25.197215271453384!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f43348a67e24b%3A0xff45e502e1ceb7e2!2sBurj%20Khalifa!5e0!3m2!1sen!2s!4v1707829118118!5m2!1sen!2s" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe title='map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d100939.9964112016!2d-122.52000086435882!3d37.75780703728917!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80859a6d00690021%3A0x4a501367f076adff!2sSan%20Francisco%2C%20CA%2C%20USA!5e0!3m2!1sen!2s!4v1707902661404!5m2!1sen!2s" loading="lazy" ></iframe>
             </div>
         </div>
       </div>
