18d7bb14da9f75b5dc6b5fb87680089011e2a1f0
AnasMehmood1 <anasmehmood167@gmail.com>
2024-02-22 21:23:09 +0500

    anasmehmood

--- src/Component/Hero/OtherHero.jsx
+++ src/Component/Hero/OtherHero.jsx
@@ -7,8 +7,13 @@ const OtherHero = ({current ,title}) => {
      <div className="other-hero">
      <div className="inner">
      <div className="links">
-       <Link to='/' className='link'><p>Home <i class="fa-solid fa-angle-right"></i></p></Link>
-       <Link className='link'>  <p>{current} <i class="fa-solid fa-angle-right"></i></p></Link>
+      <div className="home-link">
+        <Link className='Link' to='/'><p>Home</p></Link>
+        <i class="fa-solid fa-angle-right"></i>
+      </div>
+      <div className="page-link">
+      <Link className='Link'>  <p>{current} <i class="fa-solid fa-angle-right"></i></p></Link> 
+      </div>
        </div>
        <div className="title">
         <h1>{title}</h1>
